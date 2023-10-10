4a1165bb61b849a5a7aae12a0e2a90091a2c42db
priyashan <shanmugapriyashanu2002@gmail.com>
2023-10-10 12:08:26 +0530

    Initial Commit

---
+++ ShanmugaPriya-SwiftUI-Project/ShanmugaPriya_SwiftUI_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShanmugaPriya_SwiftUI_ProjectApp.swift
+//  ShanmugaPriya-SwiftUI-Project
+//
+//  Created by Shanmuga Priya M on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ShanmugaPriya_SwiftUI_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
